31302c01fdfd80e0df78c51f852d4a10e2fa17e9
T3AM-ALONE-FIGHTER <85090458+T3AM-ALONE-FIGHTER@users.noreply.github.com>
2021-05-31 19:51:09 +0530

    Update repo.py

--- userbot/plugins/repo.py
+++ userbot/plugins/repo.py
@@ -16,4 +16,4 @@ from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
 @bot.on(sudo_cmd(pattern=f"repo", allow_sudo=True))
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await edit_or_reply(e, "[Click here](https://github.com/T3AM-ELITES/ELITES-SPAM-BOT) to open this\n🔥**Lit AF!!**🔥__𝐄𝐋𝐈𝐓𝐄𝐒__ repo.\nJoin [support](t.me/elites_userbot) group)")
+        await edit_or_reply(e, "[Click here]() to open this\n🔥**Lit AF!!**🔥__ALONEhttps://github.com/T3AM-ALONE-FIGHTER/ALONE-FIGHTER-BOT__ repo.\nJoin [support](https://t.me/ALONE_FIGHTER_USERBOT) group)")
